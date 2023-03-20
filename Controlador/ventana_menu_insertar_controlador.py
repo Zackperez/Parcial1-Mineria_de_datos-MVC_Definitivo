d85b8954055b341670bda88bb76db9ec7fe76d45
Zacks <53922473+Zackperez@users.noreply.github.com>
2023-03-20 11:38:07 -0500

    Add files via upload

--- Controlador/ventana_menu_insertar_controlador.py
+++ Controlador/ventana_menu_insertar_controlador.py
@@ -21,11 +21,13 @@ class Ventana_Menu_Insertar_Controlador():
         )
 
         if len(self.view.txt_proyecto.get()) != 0 and len(self.view.txt_tipo.get()) != 0 and len(self.view.txt_capacidad.get()) != 0 and len(self.view.txt_departamento.get()) != 0 and len(self.view.txt_municipio.get()) != 0 and len(self.view.txt_cod_departamento.get()) != 0 and len(self.view.cod_municipio.get()) != 0 and len(self.view.txt_fecha.get()) != 0 and len(self.view.txt_energia.get()) != 0 and len(self.view.txt_usuarios.get()) != 0 and len(self.view.txt_inversion.get()) !=0 and len(self.view.txt_empleos_estimados.get()) != 0 and len(self.view.emisiones.get()) != 0:
-            fcursor=bd.cursor()
-            
-            sql="INSERT INTO metafncer (Proyecto, Tipo, Capacidad, Departamento, Municipio, Codigo_Departamento, Codigo_Municipio, Fecha_Estimada_FPO, Energia, Usuarios, Inversion_Estimada, Empleos_estimados, Emisiones_CO2) VALUES('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}', '{12}' )".format(self.view.txt_proyecto.get(), self.view.txt_tipo.get(), self.view.txt_capacidad.get(), self.view.txt_departamento.get(), self.view.txt_municipio.get(), self.view.txt_cod_departamento.get(), self.view.cod_municipio.get(), self.view.txt_fecha.get(), self.view.txt_energia.get(), self.view.txt_usuarios.get(), self.view.txt_inversion.get(), self.view.txt_empleos_estimados.get(), self.view.emisiones.get()) 
 
             try:
+
+                fcursor=bd.cursor()
+            
+                sql="INSERT INTO metafncer (Proyecto, Tipo, Capacidad, Departamento, Municipio, Codigo_Departamento, Codigo_Municipio, Fecha_Estimada_FPO, Energia, Usuarios, Inversion_Estimada, Empleos_estimados, Emisiones_CO2) VALUES('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}', '{12}' )".format(self.view.txt_proyecto.get(), self.view.txt_tipo.get(), self.view.txt_capacidad.get(), self.view.txt_departamento.get(), self.view.txt_municipio.get(), self.view.txt_cod_departamento.get(), self.view.cod_municipio.get(), self.view.txt_fecha.get(), self.view.txt_energia.get(), self.view.txt_usuarios.get(), self.view.txt_inversion.get(), self.view.txt_empleos_estimados.get(), self.view.emisiones.get()) 
+
                 fcursor.execute(sql)
                 bd.commit()
                 messagebox.showinfo(message="Registro exitoso", title="Aviso")
